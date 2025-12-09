233eabf9a48cb5da482bfdad6639fe49b3beace0
Sanjeev Kumar <mymail2005sanjeev@gmail.com>
2025-12-09 10:27:48 +0530

    Add files via upload

---
+++ SQL9.sql
@@ -0,0 +1,20 @@
+select database();
+show tables;
+describe city; 
+select * from city; -- to print data of a table
+
+select name, population from city; -- particular column names
+
+select population, name, district from city; -- column means order will unordered
+
+select population, population+10, name, district from city; -- never change in original table only print
+
+-- kaise bhi likhe but commands sahi hone chahie
+-- Not case sensetive, no any indention or format
+
+select population, name, district from city; 
+
+
+select *, population, population+12-022-332-82 from city;
+
+
